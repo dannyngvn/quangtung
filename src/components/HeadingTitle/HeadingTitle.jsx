56501fb39f035feabf218c9721a3cf1614f01521
bie <dannyngvn@gmaile.com>
2023-05-26 22:05:54 +0700

    update title heading

---
+++ src/components/HeadingTitle/HeadingTitle.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './HeadingTitle.css';
+const HeadingTitle = () => {
+  return (
+    <div className="heading-title container">
+      <div className="heading-line"></div>
+      <h2 className="title">Title</h2>
+      <div className="heading-line"></div>
+    </div>
+  );
+};
+
+export default HeadingTitle;
--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -7,11 +7,10 @@ import SelectionSmall from './SelectionSmall/SelectionSmall';
 import CountdownTimer from './CountdownTimer/CountdownTimer';
 import Slide from './Slider/Slider';
 import SlideSelf from './Slider/SliderSelf';
-import { NavLink } from "react-router-dom";
-import Services from "../Services";
-import Trusted from "../Trusted";
-
-
+import { NavLink } from 'react-router-dom';
+import Services from '../Services';
+import Trusted from '../Trusted';
+import HeadingTitle from '../HeadingTitle/HeadingTitle';
 
 // import { Helmet } from 'react-helmet';
 
@@ -19,32 +18,32 @@ const HomePage = () => {
   return (
     <div className="homepage">
       <Banner />
+      <HeadingTitle />
 
-      <div className="slogan-container"
-          style={{
-            backgroundImage: `url('./asset/images/slogan.jpg')`,
-            backgroundRepeat: 'no-repeat',
-            backgroundSize: 'cover',
-            height: '700px',
-            position: 'relative',
-            backgroundPosition: 'center',
-          }}
-        >
-         <div className="slogan-text">
+      <div
+        className="slogan-container"
+        style={{
+          backgroundImage: `url('./asset/images/slogan.jpg')`,
+          backgroundRepeat: 'no-repeat',
+          backgroundSize: 'cover',
+          height: '700px',
+          position: 'relative',
+          backgroundPosition: 'center',
+        }}
+      >
+        <div className="slogan-text">
           <h3>
             “CHIẾC THẢM YOGA TỐT KẾT NỐI TÂM HỒN VÀ THỂ XÁC, GIÚP BẠN LUÔN KHỎE
             MẠNH, HẠNH PHÚC”
           </h3>
 
-                <NavLink to="/products" state={{ element: "Products" }}>
-                <div className="btnn" id="btnn">
-                  <h3>SHOP NOW</h3>
-                </div>
-               </NavLink>
-          </div>
+          <NavLink to="/products" state={{ element: 'Products' }}>
+            <div className="btnn" id="btnn">
+              <h3>SHOP NOW</h3>
+            </div>
+          </NavLink>
         </div>
-        
-
+      </div>
 
       <div className="d-flex ">
         <SelectionSmall>
@@ -57,12 +56,11 @@ const HomePage = () => {
                 trang phục phù hợp để giờ tập luyện của mình được trọn vẹn.
               </p>
 
-              <NavLink to="/products" state={{ element: "Products" }}>
+              <NavLink to="/products" state={{ element: 'Products' }}>
                 <div className="btnn" id="btnn">
                   <h3>SHOP NOW</h3>
                 </div>
-               </NavLink>
-
+              </NavLink>
             </div>
           </SmallBanner>
         </SelectionSmall>
@@ -70,22 +68,17 @@ const HomePage = () => {
           <SmallBanner url="./asset/images/video1.gif">
             <div className="conten-small-banner conten-small-banner-2">
               <h1>HOT TREND – XU HƯỚNG</h1>
-              <p>
-              Xin mời cập nhật xu hướng thời trang mới tại đây!{' '}
-              </p>
+              <p>Xin mời cập nhật xu hướng thời trang mới tại đây! </p>
 
-             
-              <NavLink to="/products" state={{ element: "Products" }}>
+              <NavLink to="/products" state={{ element: 'Products' }}>
                 <div className="btnn" id="btnn">
                   <h3>SHOP NOW</h3>
                 </div>
-               </NavLink>
-            
+              </NavLink>
             </div>
           </SmallBanner>
         </SelectionSmall>
       </div>
-
 
       <div
         style={{
@@ -114,7 +107,6 @@ const HomePage = () => {
           <SlideSelf />
         </div>
       </div>
-
 
       <Services />
       <Trusted />
