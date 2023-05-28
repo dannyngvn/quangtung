61aae91b21fd45ea6c86004ba6cf42e0c8d3decb
dannyngvn <dannyngvn@gmail.com>
2023-05-28 22:06:38 +0700

    fix lỗi title và thêm đường dẫn cho grid

--- src/components/HomePage/Banner/Banner.js
+++ src/components/HomePage/Banner/Banner.js
@@ -1,18 +1,18 @@
-import "./Banner.css";
-import moon270 from "./moon270.mp4";
-import { NavLink } from "react-router-dom";
-import Product from "../../Product";
+import './Banner.css';
+import moon270 from './moon270.mp4';
+import { NavLink, Link } from 'react-router-dom';
+import Product from '../../Product';
 
 const Banner = () => {
   return (
     <>
       <video src={moon270} autoPlay loop muted />
       <div className="conten-banner">
-        <NavLink to="/products" state={{ element: "Products" }}>
-          <div className="btnn" id="btnn">
-            <h3>SHOP NOW</h3>
-          </div>
-        </NavLink>
+        <div className="btnn" id="btnn">
+          <h3>
+            <Link to="singleproduct/16">Shop Now</Link>
+          </h3>
+        </div>
       </div>
     </>
   );
