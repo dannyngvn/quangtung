2719150e785facf926deef5e1c79d488bed65c0b
phuongpv666 <36188595+phuongpv666@users.noreply.github.com>
2023-05-29 22:04:34 +0700

    Delete src directory

--- src/Helpers/FormatPrice.js
+++
@@ -1,9 +0,0 @@
-const FormatPrice = ({ price }) => {
-  return Intl.NumberFormat("vi-VN", {
-    style: "currency",
-    currency: "VND",
-    minimumFractionDigits: 0,
-  }).format(price);
-};
-
-export default FormatPrice;
--- src/components/HomePage/SelectionSmall/SelectionSmall.jsx
+++
@@ -1,7 +0,0 @@
-import './SelectionSmall.css';
-
-const SelectionSmall = props => {
-  return <div className="SelectionSmall">{props.children}</div>;
-};
-
-export default SelectionSmall;
